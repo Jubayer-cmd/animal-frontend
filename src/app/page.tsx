5a68f10102e87d3ce2719f80e1db612aa8625f99
Jubayer-cmd <sheikhabujubayer@gmail.com>
2024-03-21 16:46:37 +0600

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,12 +16,20 @@ export type Animal = {
   image: string;
   categoryId: string;
 };
+/**
+ * Represents the Home component.
+ * This component fetches categories and animals data from an API and displays them.
+ */
 export default function Home(): JSX.Element {
+  // State variables
   const [categories, setCategories] = useState<Category[]>([]);
   const [activeCategory, setActiveCategory] = useState<number | null>(null);
   const [animals, setAnimals] = useState<Animal[]>([]);
   const [filteredAnimals, setFilteredAnimals] = useState<Animal[]>([]);
 
+  /**
+   * Fetches categories and animals data from the API.
+   */
   const fetchData = useCallback(() => {
     axios
       .get('https://antropolis.vercel.app/api/V1/categories')
@@ -43,16 +51,12 @@ export default function Home(): JSX.Element {
   }, [fetchData]);
 
   useEffect(() => {
-    if (activeCategory !== null) {
-      filterAnimals(categories[activeCategory]?._id);
-    } else {
-      setFilteredAnimals(animals);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [activeCategory, categories, animals]);
-
-  const filterAnimals = useCallback(
-    (categoryId: string | null) => {
+    /**
+     * Filters the animals based on the active category.
+     * If no active category is selected, all animals are displayed.
+     * @param categoryId - The ID of the category to filter by.
+     */
+    const filterAnimals = (categoryId: string | null) => {
       if (categoryId !== null) {
         const filtered = animals.filter(
           (animal) => animal.categoryId === categoryId,
@@ -61,10 +65,20 @@ export default function Home(): JSX.Element {
       } else {
         setFilteredAnimals(animals);
       }
-    },
-    [animals],
-  );
+    };
 
+    if (activeCategory !== null) {
+      filterAnimals(categories[activeCategory]?._id);
+    } else {
+      setFilteredAnimals(animals);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [activeCategory, categories, animals]);
+
+  /**
+   * Handles the click event of a category button.
+   * @param index - The index of the clicked category button.
+   */
   const handleClick = (index: number) => {
     setActiveCategory((prevIndex) => (prevIndex === index ? null : index));
   };
@@ -73,6 +87,7 @@ export default function Home(): JSX.Element {
     <main className='container py-20 bg-black text-white min-h-screen'>
       <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
         <div className='lg:col-span-2 space-x-5'>
+          {/* Render category buttons */}
           {categories.map((category, index) => (
             <button
               className={`my-1 border px-5 py-2 rounded-full ${
@@ -88,12 +103,14 @@ export default function Home(): JSX.Element {
           ))}
         </div>
         <div className='justify-self-start lg:justify-self-end space-x-4 grid-cols-1'>
+          {/* Render modals */}
           <AddAnimalModal categories={categories} fetchData={fetchData} />
           <AddCategoryModal fetchData={fetchData} />
         </div>
       </div>
       <div>
         <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5 mt-20'>
+          {/* Render animals */}
           {filteredAnimals.map((animal) => (
             <div key={animal._id} className='flex flex-col items-center'>
               <div className='bg-[#050505] w-[160px] h-[190px] rounded-md flex items-center justify-center'>
